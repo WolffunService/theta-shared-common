12b3d25cc9eafd48c2e1f3ce0232c1415d6a618c
Tinh <minhtinh@wolffungame.com>
2021-08-11 16:58:07 +0700

    in correct userid

--- common/error_code.go
+++ common/error_code.go
@@ -1,4 +1,5 @@
 package common
+
 const (
 
 	//common code 1 - 1000
@@ -6,14 +7,16 @@ const (
 	TokenExpired = 2
 
 	//wolffunId ERROR_CODE 2001 -> 3000:
-	EmailNotExist = 2001
-	EmailIsExist  = 2002
-	CodeNotValid  = 2003
-	EmailNotValid = 2004
+	EmailNotExist   = 2001
+	EmailIsExist    = 2002
+	CodeNotValid    = 2003
+	EmailNotValid   = 2004
+	InCorrectUserId = 2005
 
 	//theta data code 3001 -> 4000
 	IdInvalid = 3001
 )
+
 var errorText = map[int]string{
 	//common code 1 - 1000
 	TokenInvalid: "Token Invalid",
@@ -21,14 +24,15 @@ var errorText = map[int]string{
 
 	//wolffunId ERROR_CODE 2001 -> 3000:
 	EmailNotExist: "Email Not Exist In Database",
-	EmailIsExist: "Email Exist In Database",
-	CodeNotValid: "Code Not Valid",
+	EmailIsExist:  "Email Exist In Database",
+	CodeNotValid:  "Code Not Valid",
 	EmailNotValid: "Email Not Valid",
+	InCorrectUserId: "Incorrect User Id",
 
 	//theta data code 3001 -> 4000
 	IdInvalid: "Id Not Valid",
-
 }
+
 // StatusText returns a text for the common error code. It returns the empty
 // string if the code is unknown.
 func ErrorText(code int) string {
