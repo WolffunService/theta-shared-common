9a4da52350bac2d4763a01c7c8201ac5ea0c6c3b
Tinh <minhtinh@wolffungame.com>
2021-08-11 15:47:20 +0700

    fix response

--- common/error_code.go
+++ common/error_code.go
@@ -1,11 +1,30 @@
 package common
 const (
+
+	//common code 1 - 1000
+	TokenInvalid = 1
+	TokenExpired = 2
+
+	//wolffunId ERROR_CODE 2001 -> 3000:
 	EmailNotExist = 2001
 	EmailIsExist  = 2002
 	CodeNotValid  = 2003
 	EmailNotValid = 2004
 
-	//Token
-	TokenInvalid = 2100
-	TokenExpired = 2101
 )
+var errorText = map[int]string{
+	//common code 1 - 1000
+	TokenInvalid: "Token Invalid",
+	TokenExpired: "Token Expired",
+
+	//wolffunId ERROR_CODE 2001 -> 3000:
+	EmailNotExist: "Email Not Exist In Database",
+	EmailIsExist: "Email Exist In Database",
+	CodeNotValid: "Code Not Valid",
+	EmailNotValid: "Email Not Valid",
+}
+// StatusText returns a text for the common error code. It returns the empty
+// string if the code is unknown.
+func ErrorText(code int) string {
+	return errorText[code]
+}
--- common/response.go
+++ common/response.go
@@ -1,7 +1,5 @@
 package common
 
-import "net/http"
-
 // Response is the response that represents an error.
 type Response struct {
 	Success       bool        `json:"success,omitempty"`
@@ -37,7 +35,8 @@ func (e Response) Error() string {
 	return e.Message
 }
 
-func ErrorResponse(code int, debugMessage string, message string) *Response {
+func ErrorResponse(code int, message string) *Response {
+	debugMessage := ErrorText(code)
 	return &Response{
 		Success:      false,
 		Code:         code,
@@ -62,98 +61,3 @@ func SuccessResponseWithPaging(data interface{}, page *Paging) ResponseWithPagin
 	response.Page = page
 	return response
 }
-
-// InternalServerError creates a new error response representing an internal server error (HTTP 500)
-func InternalServerError(msg string) Response {
-	if msg == "" {
-		msg = "We encountered an error while processing your request."
-	}
-	return Response{
-		Code:         http.StatusInternalServerError,
-		DebugMessage: http.StatusText(http.StatusInternalServerError),
-		Message:      msg,
-	}
-}
-
-// NotFound creates a new error response representing a resource-not-found error (HTTP 404)
-func NotFound(msg string) Response {
-	if msg == "" {
-		msg = "The requested resource was not found."
-	}
-	return Response{
-		Code:         http.StatusNotFound,
-		DebugMessage: http.StatusText(http.StatusNotFound),
-		Message:      msg,
-	}
-}
-
-// Unauthorized creates a new error response representing an authentication/authorization failure (HTTP 401)
-func Unauthorized(msg string) *Response {
-	if msg == "" {
-		msg = "You are not authenticated to perform the requested action."
-	}
-	return &Response{
-		Code:         http.StatusUnauthorized,
-		DebugMessage: http.StatusText(http.StatusUnauthorized),
-		Message:      msg,
-	}
-}
-func UnauthorizedRoot(err error) *Response {
-	return &Response{
-		Code:         http.StatusUnauthorized,
-		DebugMessage: http.StatusText(http.StatusUnauthorized),
-		Message:      "You are not authenticated to perform the requested action.",
-		RootError:    err,
-	}
-}
-
-// Forbidden creates a new error response representing an authorization failure (HTTP 403)
-func Forbidden(err error) *Response {
-	return &Response{
-		Code:         http.StatusForbidden,
-		DebugMessage: http.StatusText(http.StatusForbidden),
-		Message:      "You are not authorized to perform the requested action.",
-		RootError:    err,
-	}
-}
-
-// BadRequest creates a new error response representing a bad request (HTTP 400)
-func ErrBadRequest(err error) *Response {
-	return &Response{
-		Code:         http.StatusBadRequest,
-		DebugMessage: http.StatusText(http.StatusBadRequest),
-		Message:      "Your request is in a bad format.",
-		RootError:    err,
-	}
-}
-
-// BadRequest creates a new error response representing a bad request (HTTP 400)
-func BadRequest(msg string) *Response {
-	if msg == "" {
-		msg = "Your request is in a bad format."
-	}
-	return &Response{
-		Code:         http.StatusBadRequest,
-		DebugMessage: http.StatusText(http.StatusBadRequest),
-		Message:      msg,
-	}
-}
-
-func TokenValid(err error, msg string, rootCode int) *Response {
-	if msg == "" {
-		msg = "Your token is invalid."
-	}
-	return &Response{
-		Code:          http.StatusUnauthorized,
-		DebugMessage:  http.StatusText(http.StatusUnauthorized),
-		Message:       msg,
-		RootErrorCode: rootCode,
-		RootError:     err,
-	}
-}
-
-type invalidField struct {
-	Field string `json:"field"`
-	Error string `json:"error"`
-}
-
