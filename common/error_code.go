6d3b29256d8f340eb95c6404c94e9cccae5172e9
Tinh <minhtinh@wolffungame.com>
2021-08-30 14:49:55 +0700

    update new error code

--- common/error_code.go
+++ common/error_code.go
@@ -5,6 +5,7 @@ const (
 	//common code 1 - 1000
 	TokenInvalid = 1
 	TokenExpired = 2
+	UserNotFound = 3
 
 	//wolffunId ERROR_CODE 2001 -> 3000:
 	EmailNotExist   = 2001
@@ -12,6 +13,7 @@ const (
 	CodeNotValid    = 2003
 	EmailNotValid   = 2004
 	InCorrectUserId = 2005
+	NotEnoughTicketChangeName = 2006
 
 	//theta data code 3001 -> 4000
 	IdInvalid = 3001
@@ -21,6 +23,7 @@ var errorText = map[int]string{
 	//common code 1 - 1000
 	TokenInvalid: "Token Invalid",
 	TokenExpired: "Token Expired",
+	UserNotFound: "User Not Found",
 
 	//wolffunId ERROR_CODE 2001 -> 3000:
 	EmailNotExist: "Email Not Exist In Database",
@@ -28,6 +31,7 @@ var errorText = map[int]string{
 	CodeNotValid:  "Code Not Valid",
 	EmailNotValid: "Email Not Valid",
 	InCorrectUserId: "Incorrect User Id",
+	NotEnoughTicketChangeName: "User Not Enough Ticket Change Name",
 
 	//theta data code 3001 -> 4000
 	IdInvalid: "Id Not Valid",
