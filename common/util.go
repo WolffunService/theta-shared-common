bddf97bc299c3fe75789d75c306b249db5007b8f
tinh98 <>
2022-03-28 11:35:11 +0700

    add recover

--- common/util.go
+++ common/util.go
@@ -1,5 +1,10 @@
 package common
 
+import (
+	"github.com/go-errors/errors"
+	"log"
+)
+
 type UserRole int
 
 const (
@@ -7,3 +12,9 @@ const (
 	ADMIN UserRole = 1
 	ROOT  UserRole = 2
 )
+
+func Recover() {
+	if err := recover(); err != nil {
+		log.Println("recovered from ", errors.Wrap(err, 2).ErrorStack())
+	}
+}
