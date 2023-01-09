a0effc2d79e12af6784de1e1899f2752045635d6
Phước Trung <trungdlp@wolffungame.com>
2023-01-09 18:05:01 +0700

    pick multiple items with expected rate

--- common/rand/rand_expect_rate.go
+++ common/rand/rand_expect_rate.go
@@ -7,6 +7,7 @@ import (
 
 var ErrEmptyExpectRate = errors.New("empty expect rate")
 var ErrRateOutOfRange = errors.New("rate must in range 0.0 -> 1.0")
+var ErrPickTooMuch = errors.New("number item pick too much")
 
 func PickItemByExpectRate[K comparable](appearedItem map[K]int, expectRate map[K]float64) (K, error) {
 	if len(expectRate) == 0 {
@@ -45,4 +46,58 @@ func PickItemByExpectRate[K comparable](appearedItem map[K]int, expectRate map[K
 	}
 
 	return itemMinRate, nil
+}
+
+func PickMultipleItemsByExpectRate[K comparable](appearedItem map[K]int, expectRate map[K]float64, nPick int) ([]K, error) {
+	if len(expectRate) == 0 {
+		return nil, ErrEmptyExpectRate
+	}
+
+	if nPick > len(appearedItem) {
+		return nil, ErrPickTooMuch
+	}
+
+	totalAppeared := 0
+	tempAppeared := make(map[K]int, len(appearedItem))
+	for item, nAppeared := range appearedItem {
+		tempAppeared[item] = nAppeared
+		totalAppeared += nAppeared
+	}
+
+	// Calculate (Actual Rate - Expect Rate)
+	currentRate := make(map[K]float64)
+
+	var actualRate float64
+	for item, rate := range expectRate {
+		if rate < 0.0 || rate > 1.0 {
+			return nil, ErrRateOutOfRange
+		}
+
+		actualRate = 0.0
+		if totalAppeared != 0 {
+			actualRate = float64(tempAppeared[item]) / float64(totalAppeared)
+		}
+		currentRate[item] = actualRate - rate
+	}
+
+	pickedItems := make([]K, 0, nPick)
+	var minRate = math.MaxFloat64
+	var itemMinRate K
+	for i := 0; i < nPick; i++ {
+		for item, rate := range currentRate {
+			if rate < minRate {
+				minRate = rate
+				item := item
+				itemMinRate = item
+			}
+		}
+
+		pickedItems = append(pickedItems, itemMinRate)
+
+		// reset min
+		minRate = math.MaxFloat64
+		delete(currentRate, itemMinRate)
+	}
+
+	return pickedItems, nil
 }
--- common/rand/rand_expect_rate_test.go
+++ common/rand/rand_expect_rate_test.go
@@ -64,3 +64,65 @@ func TestPickItemByExpectRateStats(t *testing.T) {
 		t.Logf("Item %s appeared %d times, expect rate: %.2f%%", item, appeared, expectedRate[item]*100)
 	}
 }
+
+func TestPickMultipleItemsByExpectRate(t *testing.T) {
+	poolItems := map[string]int{
+		"apple":     1,
+		"banana":    1,
+		"raspberry": 1,
+	}
+
+	expectedRate := map[string]float64{
+		"apple":     0.5,
+		"banana":    0.3,
+		"raspberry": 0.2,
+	}
+
+	items, err := PickMultipleItemsByExpectRate(poolItems, expectedRate, 2)
+	if err != nil {
+		t.Error(err)
+	}
+
+	t.Log("Pick item is:", items)
+
+	if items[0] != "apple" || items[1] != "banana" {
+		t.FailNow()
+	}
+}
+
+func TestPickMultipleItemsByExpectRateStats(t *testing.T) {
+	poolItems := map[string]int{
+		"apple":     0,
+		"banana":    0,
+		"raspberry": 0,
+	}
+
+	expectedRate := map[string]float64{
+		"apple":     0.5, // 50%
+		"banana":    0.3, // 30%
+		"raspberry": 0.2, // 20%
+	}
+
+	numTest := 1_000_000
+	mu := sync.Mutex{}
+	count := 0
+
+	for i := 0; i < numTest; i++ {
+		items, err := PickMultipleItemsByExpectRate(poolItems, expectedRate, 1)
+		if err != nil {
+			t.Error(err)
+		}
+
+		for _, item := range items {
+			mu.Lock()
+			poolItems[item]++
+			count++
+			mu.Unlock()
+		}
+	}
+
+	t.Log("Total rand:", count)
+	for item, appeared := range poolItems {
+		t.Logf("Item %s appeared %d times, expect rate: %.2f%%", item, appeared, expectedRate[item]*100)
+	}
+}
